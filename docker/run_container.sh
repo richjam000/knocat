d15ed937f3e20925aa7419cf90c1e3ff6c48c3bb
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-20 23:57:57 +0100

    --restart=always

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -2,7 +2,7 @@ docker volume create knocat_html
 docker volume create knocat_cgi-bin
 docker volume create knocat_logs
 docker volume create knocat_conf
-docker run -d --name=knocat --network=jamnet  --hostname=knocat \
+docker run -d --name=knocat --network=jamnet  --hostname=knocat --restart=always \
 --volume knocat_html:/var/www/html \
 --volume knocat_cgi-bin:/var/www/cgi-bin \
 --volume knocat_logs:/var/log/httpd \
